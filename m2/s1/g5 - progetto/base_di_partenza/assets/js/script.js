a68e712d826333641276f951c69a54fd13639198
Luca Grossi <lucagrossi87@gmail.com>
2024-02-23 17:04:08 +0100

    progetto-m2s1g5-animationFinalFix

--- m2/s1/g5 - progetto/base_di_partenza/assets/js/script.js
+++ m2/s1/g5 - progetto/base_di_partenza/assets/js/script.js
@@ -1,13 +1,15 @@
-
 let header = document.querySelector('header');
 document.addEventListener ('scroll', colorFade)
 
+let check = false
 
 function colorFade () {
     let scrollPoint=document.documentElement.scrollTop;
-    if (scrollPoint > 400) {
-        header.style.animation="changeColorDown 1s 1 forwards"
-    } else if (scrollPoint < 400) {
+    if (scrollPoint < 400 && check) {
         header.style.animation="changeColorUp 1s 1 forwards"
+        check = false
+    } else if (scrollPoint >= 400) {
+        header.style.animation="changeColorDown 1s 1 forwards"
+        check = true
     }
 }
