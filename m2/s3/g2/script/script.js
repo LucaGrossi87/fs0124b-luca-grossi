f22cb4c112b32febd7f8c1043e3c4053903466ad
Luca Grossi <lucagrossi87@gmail.com>
2024-03-05 14:38:45 +0100

    consegna-m2s3g2

---
+++ m2/s3/g2/script/script.js
@@ -0,0 +1,34 @@
+let nameString = document.querySelector('#name')
+
+function sendToStorage(event) {
+    event.preventDefault()
+    localStorage.setItem('nome',nameString.value)
+    document.querySelector('.lastName').innerHTML=''
+    document.querySelector('.lastName').append(nameString.value)
+}
+
+function deleteFromStorage(event) {
+    event.preventDefault()
+    localStorage.removeItem('nome')
+}
+
+document.querySelector('#submit').addEventListener('click',sendToStorage)
+document.querySelector('#delete').addEventListener('click',deleteFromStorage)
+
+//-------------------------------------------------
+
+let counter = document.querySelector('.counter')
+
+function keepCount () {
+    if (sessionStorage.getItem('conteggio')){
+    counter.innerHTML=sessionStorage.getItem('conteggio')
+    }
+}
+window.addEventListener ("load", keepCount())
+
+function counterAdd() {
+    counter.innerHTML=parseInt(counter.innerHTML)+1
+    sessionStorage.setItem('conteggio', counter.innerHTML)
+}
+
+setInterval(counterAdd, 1000)
