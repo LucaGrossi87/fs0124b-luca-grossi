db1b7fb49021b2eb663c3df04d156e0fe4e5a52e
Luca Grossi <lucagrossi87@gmail.com>
2024-03-06 16:18:44 +0100

    consgna-m2s3g3-almostDone

--- m2/s3/g3/script/script.js
+++ m2/s3/g3/script/script.js
@@ -1,9 +1,31 @@
 fetch ('https://striveschool-api.herokuapp.com/books')
 .then(response => response.json())
 .then(dati => {
+    
+    let titleStorage = []
+        
+    for (let i = 0; i < sessionStorage.length; i++) {
+        titleStorage.push(sessionStorage.key(i))
+    }
 
-    console.log(dati[0].title);
+    window.addEventListener ('click', keepCount())
 
+    function keepCount () {
+        for (let i = 0; i < titleStorage.length; i++) {
+            let el = titleStorage[i];
+            let cartItem = document.createElement('li')
+            cartItem.innerHTML=el
+            document.querySelector('.cart-list').appendChild(cartItem)
+            let removeFromCart = document.createElement ('button')
+            removeFromCart.classList.add("remove-from-cart")
+            removeFromCart.innerHTML='Rimuovi dal carrello'
+            cartItem.append(removeFromCart)
+            document.querySelector('.remove-from-cart').onclick = function() {
+                this.parentElement.remove()
+            }
+        }
+    }
+        
     for(let book of dati){
 
         let template = document.getElementsByTagName('template')[0];
@@ -17,12 +39,10 @@ fetch ('https://striveschool-api.herokuapp.com/books')
             this.parentElement.parentElement.remove()
         }
         clone.querySelector('.addToCart').onclick = function() {
-            let bookTitle = this.parentElement.querySelector('.card-title').innerHTML
-            let bookPrice = this.parentElement.querySelector('.card-text').innerHTML
-            document.querySelector('.bill').innerHTML=parseInt(document.querySelector('.bill').innerHTML)+parseInt(bookPrice)
             let cartItem = document.createElement('li')
-            cartItem.innerHTML=bookTitle
+            cartItem.innerHTML=this.parentElement.querySelector('.card-title').innerHTML
             document.querySelector('.cart-list').appendChild(cartItem)
+            sessionStorage.setItem(cartItem.innerHTML, cartItem.innerHTML)
             let removeFromCart = document.createElement ('button')
             removeFromCart.classList.add("remove-from-cart")
             removeFromCart.innerHTML='Rimuovi dal carrello'
