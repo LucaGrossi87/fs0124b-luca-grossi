9162aa0944aa81124a1959daf1e304099473efec
Luca Grossi <lucagrossi87@gmail.com>
2024-03-06 15:27:55 +0100

    consegna-m2s3g3-nonrequestedExtra

--- m2/s3/g3/script/script.js
+++ m2/s3/g3/script/script.js
@@ -18,14 +18,22 @@ fetch ('https://striveschool-api.herokuapp.com/books')
         }
         clone.querySelector('.addToCart').onclick = function() {
             let bookTitle = this.parentElement.querySelector('.card-title').innerHTML
+            let bookPrice = this.parentElement.querySelector('.card-text').innerHTML
+            document.querySelector('.bill').innerHTML=parseInt(document.querySelector('.bill').innerHTML)+parseInt(bookPrice)
             let cartItem = document.createElement('li')
             cartItem.innerHTML=bookTitle
-            document.querySelector('.cartList').appendChild(cartItem)
-            
+            document.querySelector('.cart-list').appendChild(cartItem)
+            let removeFromCart = document.createElement ('button')
+            removeFromCart.classList.add("remove-from-cart")
+            removeFromCart.innerHTML='Rimuovi dal carrello'
+            cartItem.append(removeFromCart)
+            document.querySelector('.remove-from-cart').onclick = function() {
+                this.parentElement.remove()
+            }
         }
         
         let column = document.createElement('div')
-        column.classList.add("col-12", "col-md-6", "col-lg-3")
+        column.classList.add("col-12", "col-md-6", "col-lg-4", "col-xl-3")
         column.appendChild(clone)
         document.querySelector('.col-10').appendChild(column)
     }
