0a47294567651e7999044d55066aaaed28715af9
Luca Grossi <lucagrossi87@gmail.com>
2024-03-08 16:49:48 +0100

    progetto-m2s3g5-note

--- m2/s3/g5 - progetto/script/script-edit.js
+++ m2/s3/g5 - progetto/script/script-edit.js
@@ -1,5 +1,5 @@
 document.querySelector('.edit-btn').addEventListener('click', function editAmp() {
-    fetch("https://striveschool-api.herokuapp.com/api/product/", {   //devo mettere l'id del prodotto
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {   //devo mettere l'id del prodotto, "ereditandolo" dalla card dell'index, una volta uniti gli script in un unico file, stessa cosa per eliminarlo
         method:"PUT",
         headers: {
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZmEyNjJkN2IxMTAwMTkwZTcyMGIiLCJpYXQiOjE3MDk4OTgyNzgsImV4cCI6MTcxMTEwNzg3OH0.ci2OKjqfEDMKBwm6O3iD18btmFgJkiZUPijDoehdgss",
