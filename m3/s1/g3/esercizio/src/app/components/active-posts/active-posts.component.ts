68f65160665d6d5172436ddccd14a9de06a0e1a9
Luca Grossi <lucagrossi87@gmail.com>
2024-03-20 16:56:24 +0100

    consegna-m3s1g3-randomFix

--- m3/s1/g3/esercizio/src/app/components/active-posts/active-posts.component.ts
+++ m3/s1/g3/esercizio/src/app/components/active-posts/active-posts.component.ts
@@ -9,30 +9,34 @@ import { Component } from '@angular/core';
 export class ActivePostsComponent {
   title = 'EPICBlog';
 
-  postsArr:iPost[]=[]
-  dbObj:iRoot={
+  postsArr: iPost[] = [];
+  dbObj: iRoot = {
     posts: [],
     total: 0,
     skip: 0,
     limit: 0
-  }
-  randomArr:{title:string,body:string}[]=[]
-  details:{title:string, body:string}={
-    title:'',
-    body:''
-  }
+  };
+  randomArr: { title: string, body: string }[] = [];
+  details: { title: string, body: string } = {
+    title: '',
+    body: ''
+  };
 
-  ngOnInit(){
-
+  ngOnInit() {
     fetch('../../../assets/db.json')
-    .then(db => db.json())
-    .then((db:iRoot) => this.dbObj = db)
-    .then(
-      for (let i = 0; i <4; i++) {
-        let randomNum:number = Math.floor(Math.random() * this.dbObj.posts.length)
-        this.details.title=this.dbObj.posts[randomNum].title
-        this.details.body=this.dbObj.posts[randomNum].body
-        this.randomArr.push(this.details)
-      })
+      .then(db => db.json())
+      .then((db: iRoot) => {
+        this.dbObj = db;
+        this.randomize();
+      });
   }
+
+  randomize() {
+    for (let i = 0; i < 4; i++) {
+      let randomNum: number = Math.floor(Math.random() * this.dbObj.posts.length);
+      this.details.title = this.dbObj.posts[randomNum].title;
+      this.details.body = this.dbObj.posts[randomNum].body;
+      this.randomArr.push({ ...this.details });
+    }
   }
+}
