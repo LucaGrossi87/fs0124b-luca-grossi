c6b8b930b7a0927df80a8700515e602a35f67a25
Luca Grossi <lucagrossi87@gmail.com>
2024-06-21 09:52:57 +0200

    capstoneEpicode-restoreBooking

---
+++ Progetto Finale/valhallagaming/src/main/java/it/epicode/valhallagaming/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package it.epicode.valhallagaming.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
---
+++ Progetto Finale/valhallagaming/src/main/java/it/epicode/valhallagaming/service/BoardSchedulingService.java
@@ -0,0 +1,37 @@
+package it.epicode.valhallagaming.service;
+
+import it.epicode.valhallagaming.entity.Board;
+import it.epicode.valhallagaming.entity.Lan;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardSchedulingService {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private LanService lanService;
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void resetAvailability(){
+        List<Board> boardList = boardService.findAll();
+        List<Lan> lanList = lanService.findAll();
+        for (Board board : boardList){
+            board.setAvailable(true);
+            board.setOpen(false);
+            board.setSeatsAvalaible(board.getSeatsAvalaible());
+            boardService.save(board);
+        }
+        for (Lan lan : lanList){
+            lan.setAvailable(true);
+            lanService.save(lan);
+        }
+    }
+}
